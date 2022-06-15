be3ddafefe30e53d123bc6bc7d1050efa328a638
Okky Maheswara <okkymahes@gmail.com>
2022-06-15 18:11:57 +0800

    tambah page notfound

---
+++ pages/404.js
@@ -0,0 +1,10 @@
+
+const NotFound = () => {
+  return (
+    <div className="text-center text-gray-600 font-semibold mt-20 text-2xl">
+      Oops! Page Not Found 😀
+    </div>
+  )
+}
+
+export default NotFound
